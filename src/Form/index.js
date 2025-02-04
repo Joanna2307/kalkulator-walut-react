7f5a2e8ddc42baa8f2baf945ab67bf78825715e1
Joanna Gorniak <joanna.gorniak.00@gmail.com>
2025-02-04 20:32:24 +0100

    interactions updated

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,54 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
+import Calculate from "../Calculate";
+import Result from "../Result";
 
-const Form = (props) => (
-  <form className="form">
-    <fieldset className="form__fieldset">
-      <legend className="form__legend">Kalkulator walut</legend>
-      <p>
-        <label className="form__label">
-          Przelicz z:{" "}
-          <select className="form__select" name="currencyFrom">
-            <option value="PLN" selected>
-              Złoty polski (PLN)
-            </option>
-            <option value="USD">Dolar amerykański (USD)</option>
-            <option value="GBP">Funt brytyjski (GBP)</option>
-            <option value="EUR">Euro (EUR)</option>
-          </select>
-        </label>
+const Form = ({ currency }) => {
+  const [fromCurrency, setFromCurrency] = useState(currency[0].rate);
+  const [toCurrency, setToCurrency] = useState(currency[0].rate);
+  const [amount, setAmount] = useState("");
+  const [result, setResult] = useState(0);
 
-        <label className="form__label">
-          Przelicz na:{" "}
-          <select className="form__select" name="currencyTo">
-            <option value="PLN" selected>
-              Złoty polski (PLN)
-            </option>
-            <option value="USD">Dolar amerykański (USD)</option>
-            <option value="GBP">Funt brytyjski (GBP)</option>
-            <option value="EUR">Euro (EUR)</option>
-          </select>
-        </label>
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    calculateResult(amount, fromCurrency, toCurrency);
+  };
 
-        <label className="form__label">
-          Kwota:*{" "}
-          <input
-            className="form__input"
-            type="number"
-            name="amount"
-            required
-            step="0.01"
-          />
-        </label>
-      </p>
-      <p>
-        <button className="form__button">Konwertuj</button>
-      </p>
-      <p className="form__result" name="result">
-        Kwota po przeliczeniu:
-      </p>
-    </fieldset>
-    <p className="form__caption">* Pole wymagane</p>
-  </form>
-);
+  const calculateResult = (amount, fromCurrency, toCurrency) => {
+    const calculatedResult = (amount / fromCurrency) * toCurrency;
+    setResult(calculatedResult.toFixed(2));
+  };
+
+  return (
+    <form className="form" onSubmit={onFormSubmit}>
+      <fieldset className="form__fieldset">
+        <legend className="form__legend">Kalkulator walut</legend>
+
+        <div>
+          <label className="form__label">
+            Przelicz z:
+            <select
+              className="form__select"
+              name="currencyFrom"
+              value={fromCurrency}
+              onChange={(event) =>
+                setFromCurrency(parseFloat(event.target.value))
+              }
+            >
+              {currency.map((item) => (
+                <option key={item.code} value={item.rate}>
+                  {item.code}
+                </option>
+              ))}
+            </select>
+          </label>
+        </div>
+
+        <div>
+          <label className="form__label">
+            Przelicz na:
+            <select
+              className="form__select"
+              name="currencyTo"
+              value={toCurrency}
+              onChange={(event) =>
+                setToCurrency(parseFloat(event.target.value))
+              }
+            >
+              {currency.map((item) => (
+                <option key={item.code} value={item.rate}>
+                  {item.code}
+                </option>
+              ))}
+            </select>
+          </label>
+        </div>
+
+        <div>
+          <label className="form__label">
+            Kwota:*{" "}
+            <input
+              className="form__input"
+              type="number"
+              name="amount"
+              required
+              step="0.01"
+              value={amount}
+              onChange={({ target }) => setAmount(target.value)}
+            />
+          </label>
+        </div>
+
+        <div>
+          <Calculate />
+        </div>
+
+        <div className="form__result" name="result">
+          Kwota po przeliczeniu:
+          <Result result={result} />
+        </div>
+      </fieldset>
+
+      <p className="form__caption">* Pole wymagane</p>
+    </form>
+  );
+};
+
 export default Form;
